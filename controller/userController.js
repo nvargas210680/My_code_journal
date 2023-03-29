6c3125b55c32219080e84a0f2985545e8be71ed3
Nicolas Vargas <nikolas210680@gmail.com>
2023-03-28 21:31:09 -0600

    bugs_fixed

--- controller/userController.js
+++ controller/userController.js
@@ -29,14 +29,14 @@ export const show = (req, res, next) => {
 }
 
 export const store = (req, res, next) => {
-  let user = new User({
+  let user = new user({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
     journalName: req.body.journalName,
     file: req.body.file,
   });
-  User.save()
+  user.save()
     .then((response) => {
       res.json({
         message: "Journal added successfully!",
@@ -77,12 +77,12 @@ export const destroy = (req, res, next) => {
   let userID = req.body.userID;
   User.findByIdAndRemove(userID)
     .then(() => {
-      req.json({
+      res.json({
         message: "User deleted successfully!",
       });
     })
     .catch((error) => {
-      req.json({
+      res.json({
         message: "An error Occurred",
       });
     });
